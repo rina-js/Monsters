82652daabba7b632e5542c8a3d39c12509f1b024
rina <rinarivaloff@hotmail.com>
2022-12-14 00:28:15 +0300

    statements improved

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -33,40 +33,84 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
     finalMosterHealth: monsterHealth,
     flinalPlayerHealth: playerHealth,
   }
-  if (ev === LOG_EVENT_PLAYER_ATTACK) {
-    logEntry.target = "MONSTER"
-  } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-    logEntry = {
-      event: ev,
-      value: val,
-      target: "MONSTER",
-      finalMosterHealth: monsterHealth,
-      flinalPlayerHealth: playerHealth,
-    }
-  } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
-    logEntry = {
-      event: ev,
-      value: val,
-      target: "PLAYER",
-      finalMosterHealth: monsterHealth,
-      flinalPlayerHealth: playerHealth,
-    }
-  } else if (ev === LOG_EVENT_PLAYER_HEAL) {
-    logEntry = {
-      event: ev,
-      value: val,
-      target: "PLAYER",
-      finalMosterHealth: monsterHealth,
-      flinalPlayerHealth: playerHealth,
-    }
-  } else if (ev === LOG_EVENT_GAME_OVER) {
-    logEntry = {
-      event: ev,
-      value: val,
-      finalMosterHealth: monsterHealth,
-      flinalPlayerHealth: playerHealth,
-    }
+
+  switch (ev) {
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry.target = "MONSTER"
+      break
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry = {
+        event: ev,
+        value: val,
+        target: "PLAYER",
+        finalMosterHealth: monsterHealth,
+        flinalPlayerHealth: playerHealth,
+      }
+      break
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry = {
+        event: ev,
+        value: val,
+        target: "PLAYER",
+        finalMosterHealth: monsterHealth,
+        flinalPlayerHealth: playerHealth,
+      }
+      break
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry = {
+        event: ev,
+        value: val,
+        target: "PLAYER",
+        finalMosterHealth: monsterHealth,
+        flinalPlayerHealth: playerHealth,
+      }
+      break
+    case LOG_EVENT_GAME_OVER:
+      logEntry = {
+        event: ev,
+        value: val,
+        finalMosterHealth: monsterHealth,
+        flinalPlayerHealth: playerHealth,
+      }
+      break
+    default:
+      logEntry = {}
   }
+
+  // if (ev === LOG_EVENT_PLAYER_ATTACK) {
+  //   logEntry.target = "MONSTER"
+  // } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     target: "MONSTER",
+  //     finalMosterHealth: monsterHealth,
+  //     flinalPlayerHealth: playerHealth,
+  //   }
+  // } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     target: "PLAYER",
+  //     finalMosterHealth: monsterHealth,
+  //     flinalPlayerHealth: playerHealth,
+  //   }
+  // } else if (ev === LOG_EVENT_PLAYER_HEAL) {
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     target: "PLAYER",
+  //     finalMosterHealth: monsterHealth,
+  //     flinalPlayerHealth: playerHealth,
+  //   }
+  // } else if (ev === LOG_EVENT_GAME_OVER) {
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     finalMosterHealth: monsterHealth,
+  //     flinalPlayerHealth: playerHealth,
+  //   }
+  // }
   battleLog.push(logEntry)
 }
 
@@ -127,15 +171,12 @@ function endRound() {
 }
 
 function attackMonster(mode) {
-  let maxDamage
-  let logEvent
-  if (mode === MODE_ATTACK) {
-    maxDamage = ATTACK_VALUE
-    logEvent = LOG_EVENT_PLAYER_ATTACK
-  } else if (mode === MODE_STRONG_ATTACK) {
-    maxDamage = STRONG_ATTACK_VALUE
-    logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK
-  }
+  const maxDamage = mode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE
+  const logEvent =
+    mode === MODE_ATTACK
+      ? LOG_EVENT_PLAYER_ATTACK
+      : LOG_EVENT_PLAYER_STRONG_ATTACK
+
   const damage = dealMonsterDamage(maxDamage)
   currentMonsterHealth -= damage
   writeToLog(logEvent, damage, currentMonsterHealth, currentPlayerHealth)
